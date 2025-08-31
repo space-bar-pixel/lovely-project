f3518c5b11b18098864437da82b45affc53dc8aa
space-bar-pixel <nantawat1348@gmail.com>
2025-09-01 01:48:59 +0700

    Update Main.lua

--- _script/Main.lua
+++ _script/Main.lua
@@ -7,9 +7,8 @@ local placeId = tonumber(string.sub(tostring(game.PlaceId), 1, 10))
 local scriptUrl = scriptsByPlaceId[placeId]
 
 if scriptUrl then
-    print("Loading script for PlaceId:", placeId)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/space-bar-pixel/lovely-project/refs/heads/main/_script/%5Braw%5Dbuild-a-zoo.lua"))()
-    --loadstring(game:HttpGet(scriptUrl))()
+    print("!Loading script for PlaceId:", placeId)
+    loadstring(game:HttpGet(scriptUrl))()
 else
     print("No script for this PlaceId:", placeId)
 end
