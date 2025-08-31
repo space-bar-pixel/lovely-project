2ee9609a4fe01a985011dc73c70100b89cafd155
space-bar-pixel <nantawat1348@gmail.com>
2025-09-01 01:16:54 +0700

    Update Main.lua

--- _script/Main.lua
+++ _script/Main.lua
@@ -1,1 +1,7 @@
-
+if game.PlaceId == 105555311806207 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/space-bar-pixel/lovely-project/refs/heads/main/_script/build-a-zoo.lua?token=GHSAT0AAAAAADIFOEA2NGPIIUGCJXVN33UW2FUSDAQ"))()
+else if game.PlaceId == 119048529960596 then
+    
+else
+    print("This is not the target game.")
+end
