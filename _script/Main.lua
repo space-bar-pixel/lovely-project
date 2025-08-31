888891bc9a6ed728e2a212883dc5df07abdd495d
space-bar-pixel <nantawat1348@gmail.com>
2025-09-01 01:31:01 +0700

    Update Main.lua

--- _script/Main.lua
+++ _script/Main.lua
@@ -1,4 +1,4 @@
-if game.PlaceId == 1055553118 then
+if game.PlaceId == 105555311806207 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/space-bar-pixel/lovely-project/refs/heads/main/_script/build-a-zoo.lua?token=GHSAT0AAAAAADIFOEA2NGPIIUGCJXVN33UW2FUSDAQ"))()
 elseif game.PlaceId == 1190485299 then
     loadstring(game:HttpGet("https://i.uerd.de/rt3.lua"))()
