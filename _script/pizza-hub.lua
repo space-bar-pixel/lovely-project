d9ab2453f7d3626e8807316ffaedeb4348a31755
space-bar-pixel <nantawat1348@gmail.com>
2025-09-01 01:26:45 +0700

    Update pizza-hub.lua

--- _script/pizza-hub.lua
+++ _script/pizza-hub.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/space-bar-pixel/lovely-project/refs/heads/main/_script/Main.lua?token=GHSAT0AAAAAADIFOEA236LJIM7WNRVS3E6S2FUSNLA"))()
+local url = "https://raw.githubusercontent.com/space-bar-pixel/lovely-project/refs/heads/main/_script/Main.lua?token=GHSAT0AAAAAADIFOEA3VYZ7GSZ6U57XLR4U2FUSP3A"
+loadstring(game:HttpGet(url))()
