aa7cae3f1a9ab1a16992e94f7162135c05e4e25a
space-bar-pixel <nantawat1348@gmail.com>
2025-09-02 09:23:25 +0700

    Create darkDexV5.lua

---
+++ _script/darkDexV5.lua
@@ -0,0 +1,6 @@
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+-- Credits to AlterX404, and memeenjoyer43
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/memeenjoyer43/darkdex/refs/heads/main/script"))()
