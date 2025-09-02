500a3ea7167631052ad925afe53ceb91b4a9a7ad
space-bar-pixel <nantawat1348@gmail.com>
2025-09-02 17:12:51 +0700

    Update Main.lua

--- _script/Main.lua
+++ _script/Main.lua
@@ -1,6 +1,6 @@
 local scriptsByPlaceId = {
     [1055553118] = "https://raw.githubusercontent.com/space-bar-pixel/lovely-project/refs/heads/main/_script/%5Braw%5Dbuild-a-zoo.lua",
-    [1190485299] = "https://api.luarmor.net/files/v4/loaders/d7be76c234d46ce6770101fded39760c.lua"
+    [1190485299] = "https://raw.githubusercontent.com/space-bar-pixel/lovely-project/refs/heads/main/_script/RST3.lua"
 }
 
 local placeId = tonumber(string.sub(tostring(game.PlaceId), 1, 10))
